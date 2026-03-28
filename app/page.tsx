d718a60a3e0de0be156d1a087928ac76d633c312
Jean Paulo <jean.paulo@okeanyachts.com>
2026-03-28 10:30:57 -0300

    new commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import { Hero } from '@/components/hero'
 import { Services } from '@/components/services'
 import { HowItWorks } from '@/components/how-it-works'
 import { Benefits } from '@/components/benefits'
-import { Testimonials } from '@/components/testimonials'
 import { FinalCTA } from '@/components/final-cta'
 import { FloatingWhatsApp } from '@/components/floating-whatsapp'
 
@@ -15,7 +14,6 @@ export default function Home() {
       <div id="services"><Services /></div>
       <div id="how-it-works"><HowItWorks /></div>
       <div id="benefits"><Benefits /></div>
-      <div id="testimonials"><Testimonials /></div>
       <FinalCTA />
       <FloatingWhatsApp />
     </main>
